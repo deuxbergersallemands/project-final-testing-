339dcff5f24f52ac215f91b5636639df4c400721
Raphael Jorel <raphael.jorel@laposte.net>
2015-10-22 15:45:27 +0200

    [US6-S1-T20] Vue pour développeurs

---
+++ app/view/content/developers/list.php
@@ -0,0 +1,25 @@
+
+<table>
+    <tr>
+        <th>Name</th>
+        <th>First name</th>
+    </tr>
+
+<?php
+foreach ($context->getData() as $dev) {
+?>
+    <tr>
+        <td><?php echo $dev->devName; ?></td>
+        <td>
+            <a href="?developers&amp;id=<?php echo $dev->devId ?>">
+                <?php echo $dev->devFirstName; ?>
+            </a>
+        </td>
+        <td><a href="?developers&amp;edit=<?php echo $dev->devId; ?>">Edit</a></td>
+        <td><a href="?developers&amp;del=<?php echo $dev->devId; ?>">Delete</a></td>
+    </tr>
+<?php
+}
+?>
+
+</table>
