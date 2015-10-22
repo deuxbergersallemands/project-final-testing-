3f558e0eb2241da4ee2157db45ed49b23ce638ac
Raphaël Jorel <raphael.jorel@laposte.net>
2015-10-22 10:06:55 +0200

    [US2-T1-T19] Modification des champs, ajout de la description

--- app/view/content/task/edit.php
+++ app/view/content/task/edit.php
@@ -4,7 +4,7 @@
     <input type="hidden" name="edit_task_id" value="<?php echo $task->taskId; ?>" />
     <label>Identifier:</label><input type="text" name="edit_task_identifier"
                                 value="<?php echo $task->taskIdentifier; ?>" required /><br />
-    <label>Description:</label><input type="text" name="edit_task_description"
+    <label>Description:</label><input type="text" name="edit_task_summary"
                                     value="<?php echo $task->taskSummary; ?>" required /><br />
     <label>Expected duration:</label><input type="text" name="edit_task_expected_duration"
                                         value="<?php echo $task->taskExpectedDuration; ?>" /><br />
