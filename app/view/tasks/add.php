0cf1104e074c92df28914fc0fe32fd852e792aaf
mira49 <miratouatii@gmail.com>
2015-10-27 00:53:35 +0100

    Design

--- app/view/tasks/add.php
+++ app/view/tasks/add.php
@@ -1,12 +1,10 @@
 
-<form method="post" action="?tasks">
-    <label>Identifier:</label><input type="text" name="new_task_identifier" required /><br />
-    <label>Summary:</label><input type="text" name="new_task_summary" required /><br />
-    <label>Expected duration:</label><input type="text" name="new_task_expected_duration" /><br />
-    <label>Description:</label><br /><textarea name="new_task_description"></textarea><br />
-    <input type="submit" value="Add task">
+<form class="form-signin add" method="post" action="?tasks">
+    <label >Identifier:</label><input  type="text" name="new_task_identifier" required /><br />
+    <label >Summary:</label><input  type="text" name="new_task_summary" required /><br />
+    <label >Expected duration:</label><input  type="text" name="new_task_expected_duration" /><br />
+    <label >Description:</label><br /><textarea name="new_task_description"></textarea><br />
+    <button   onclick="window.location.href = '?tasks';" class="btn btn-lg "  >Cancel </button>
+    <button  class="btn btn-lg " type="submit"  >Add task </button>
 </form>
 
-
-<br />
-<a href="?tasks"><img src="<?php echo $context::BACK_BUTTON_IMG; ?>" /><button type="button" class="btn btn-link">Back</button></a><br />
--- app/view/tasks/view.php
+++ app/view/tasks/view.php
@@ -1,31 +1,31 @@
 <?php $task = $context->getData(); ?>
 
-<table>
+<table class="table-list view" >
     <tr>
-        <th>Identifier:</th>
-        <td><?php echo $task->taskIdentifier; ?></td>
+        <th >Identifier:</th>
+        <td  class="table-td"><?php echo $task->taskIdentifier; ?></td>
     </tr>
     <tr>
-        <th>Summary:</th>
-        <td><?php echo $task->taskSummary; ?></td>
+        <th >Summary:</th>
+        <td  class="table-td"><?php echo $task->taskSummary; ?></td>
     </tr>
     <tr>
-        <th>Expected duration:</th>
-        <td><?php echo $task->taskExpectedDuration; ?></td>
+        <th >Expected duration:</th>
+        <td  class="table-td"><?php echo $task->taskExpectedDuration; ?></td>
     </tr>
     <tr>
-        <th>Real duration:</th>
-        <td><?php echo $task->taskDuration; ?></td>
+        <th >Real duration:</th>
+        <td  class="table-td"><?php echo $task->taskDuration; ?></td>
     </tr>
     <tr>
-        <th>State:</th>
-        <td><?php echo $task->taskState; ?></td>
+        <th >State:</th>
+        <td class="table-td"><?php echo $task->taskState; ?></td>
     </tr>
     <tr>
-        <th>Description:</th>
-        <td><?php echo $task->taskDescription; ?></td>
+        <th >Description:</th>
+        <td  class="table-td"><?php echo $task->taskDescription; ?></td>
     </tr>
 </table>
 
 <br />
-<a href="?tasks"><img src="<?php echo $context::BACK_BUTTON_IMG; ?>" /><button type="button" class="btn btn-link">Back</button></a><br />
+    <button  class="btn btn-lg add" onclick="window.location.href = '?tasks';"  > Back</button>
