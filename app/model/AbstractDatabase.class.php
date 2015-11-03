afc673ab57289216925c076a35a8d4fa98b2acd8
Raphael Jorel <raphael.jorel@laposte.net>
2015-11-03 20:51:43 +0100

    [S2-T6] Ajout de fonctions, nouvelle archi de classes

---
+++ app/model/AbstractDatabase.class.php
@@ -0,0 +1,42 @@
+<?php
+
+class AbstractDatabase
+{
+	protected $_host = "localhost";
+	protected $_dbname = "rjorel";
+	protected $_username = "rjorel";
+	protected $_passwd = "truc";
+	protected $_db;
+
+
+	public function __construct()
+	{
+		try {
+			$this->_db = new PDO("mysql:host=$this->_host;dbname=$this->_dbname", $this->_username, $this->_passwd);
+		}
+		catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+	
+	
+	// Util functions to fetch and close queries.
+	
+	protected function fetch($req)
+	{
+		if (!$req) return false;
+	
+		$data = $req->fetch(PDO::FETCH_OBJ);
+		$req->closeCursor();
+		return $data;
+	}
+	
+	protected function fetchAll($req)
+	{
+		if (!$req) return false;
+	
+		$data = $req->fetchAll(PDO::FETCH_OBJ);
+		$req->closeCursor();
+		return $data;
+	}
+}
---
+++ app/model/UserstoryDatabase.class.php
@@ -0,0 +1,96 @@
+<?php
+
+require "model/AbstractDatabase.class.php";
+
+
+class UserstoryDabase extends AbstractDatabase
+{
+	public function getUserStories() {
+		return $this->fetchAll($this->_db->query("SELECT * FROM UserStories"));
+	}
+	
+	public function getUserStory($id)
+	{
+		$req = $this->_db->prepare("SELECT * FROM UserStories WHERE usId = ?");
+		$req->execute(array($id));
+		return $this->fetch($req);
+	}
+	
+	public function addUserStory($identifier, $summ, $prio = 0, $diff = 0, $desc = "")
+	{
+		$req = $this->_db->prepare("INSERT INTO UserStories(usIdentifier, usSummary,
+										usPriority, usDifficulty, usDescription)
+										VALUES(?, ?, ?, ?, ?)");
+		$req->execute(array($identifier, $summ, $prio, $diff, $desc));
+		$req->closeCursor();
+	}
+	
+	public function updateUserStory($id, $identifier, $summ, $prio = 0, $diff = 0, $desc = "")
+	{
+		$req = $this->_db->prepare("UPDATE UserStories SET usIdentifier = ?, usSummary = ?,
+										usPriority = ?, usDifficulty = ?,
+										usDescription = ?
+										WHERE usId = ?");
+		$req->execute(array($identifier, $summ, $prio, $diff, $desc, $id));
+		$req->closeCursor();
+	}
+	
+	public function delUserStory($id)
+	{
+		$req = $this->_db->prepare("DELETE FROM UserStories WHERE usId = ?");
+		$req->execute(array($id));
+		$req->closeCursor();
+	}
+	
+	public function setUserStoryState($id, $state)
+	{
+		$req = $this->_db->prepare("UPDATE UserStories SET usState = ? WHERE usId = ?");
+		$req->execute(array($state, $id));
+		$req->closeCursor();
+	}
+	
+	public function setUserStoryDuration($id, $duration)
+	{
+		$req = $this->_db->prepare("UPDATE UserStories SET usDuration = ? WHERE usId = ?");
+		$req->execute(array($duration, $id));
+		$req->closeCursor();
+	}
+	
+	
+	// Bindings with tasks.
+	
+	public function getUserstoriesByTask($taskId)
+	{
+		$req = $this->_db->prepare("SELECT * FROM UserStories WHERE usId in
+										(SELECT usId FROM TasksToUserStories WHERE taskId = ?)");
+		$req->execute(array($taskId));
+		return $this->fetchAll($req);
+	}
+	
+	public function addTaskToUserstory($taskId, $usId)
+	{
+		$req = $this->_db->prepare("INSERT INTO TasksToUserStories(taskId, usId)
+										VALUES(?, ?)");
+		$req->execute(array($taskId, $usId));
+		$req->closeCursor();
+	}
+	
+	public function removeTaskFromUserstory($taskId, $usId)
+	{
+		$req = $this->_db->prepare("DELETE FROM TasksToUserStories
+										WHERE taskId = ? AND usId = ?");
+		$req->execute(array($taskId, $usId));
+		$req->closeCursor();
+	}
+	
+	
+	// Bindings with sprints.
+	
+	public function getUserstoriesBySprint($sprintId)
+	{
+		$req = $this->_db->prepare("SELECT * FROM UserStories WHERE usId in
+				(SELECT usId FROM UserStoriesToSprints WHERE sprintId = ?)");
+		$req->execute(array($sprintId));
+		return $this->fetchAll($req);
+	}
+}
