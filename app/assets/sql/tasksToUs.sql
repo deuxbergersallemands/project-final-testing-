49d6faec8030282ae57c4bf1f4a2e5788f58ad64
Raphael Jorel <raphael.jorel@laposte.net>
2015-11-10 18:33:59 +0100

    foreign key

--- app/assets/sql/tasksToUs.sql
+++ app/assets/sql/tasksToUs.sql
@@ -2,5 +2,7 @@
 CREATE TABLE IF NOT EXISTS `TasksToUserStories`(
     `taskId`    INT(11) NOT NULL,
     `usId`      INT(11) NOT NULL,
-    PRIMARY KEY (`taskId`, `usId`)
+    PRIMARY KEY (`taskId`, `usId`),
+    FOREIGN KEY (`taskId`) REFERENCES Tasks(`taskId`),
+    FOREIGN KEY (`usId`) REFERENCES UserStories(`usId`)
 )
