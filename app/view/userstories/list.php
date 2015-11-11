50d9d86f08bc4db41f864f9eacb2e54546a6574e
Raphael Jorel <raphael.jorel@laposte.net>
2015-11-11 11:20:29 +0100

    [S3-VD] description du backlog (en anglais)

--- app/view/userstories/list.php
+++ app/view/userstories/list.php
@@ -51,7 +51,32 @@
 <div id="description" title="User stories">
     <p>
         <i><b>English</b></i><br />
-
+        Backlog is a user story list. A user story is a sort of scenario to
+        express easily what we expect from a project. In general case, a
+        user story is written as follows: <br />
+        
+        As a &lt; role &gt;, I want &lt; goal / desire &gt; so that &lt; benefit &gt; <br />
+        <u>For example:</u> As a developer, I want the website to make coffee. <br />
+        The last part is not really mandatory. <br />
+        
+        Each user story has priority and difficulty. The first is given by just one person,
+        called the "product owner". In general, it is the customer. The second is evaluated
+        by each person in the team, and must belongs to a modified Fibonacci number. Identifier
+        is used to quickly express a user story. <br />
+        
+        Here, you can handle user stories. To add a user story, just click on 'Add' (too easy, isn't it ?)
+        and fill the fields. Just identifier and summary are required, but comments are always welcome.
+        When a new user story is added,
+        it appears in the list (or backlog), and you can edit it, delete it, or just see its description.
+        In description, you will find all the fields that you filled previously, plus the associated tasks
+        and sprints. There are also user stories dependencies. <br />
+        Edition is like addition, it's the same form, but already filled by previously given
+        data. Identifier and summary are still required. <br />
+        You can also manage a user story, that is to say, affect user story to sprints, affect tasks
+        to user story, and manage dependencies between user stories. <br />
+        
+        Burn Down Chart is used for showing advanced work. It evolves over sprints. x-axis is for
+        sprints, and y-axis is for cumulative difficulty. It permits to predict work end.
     </p>
     <p>
         <i><b>Français</b></i><br />
